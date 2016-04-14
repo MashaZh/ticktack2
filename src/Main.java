04388a1a046ad799ed09de08525cda2c2d6638fe
MashaZh <MashaZh@yandex.ru>
2016-04-14 22:50:05 +0300

    тест1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Masha on 07.03.2016.
+ */
+public class Main {
+    public static Tricktrack tricktrack;
+
+    public static void main(String[] args) {
+
+        tricktrack = new Tricktrack();
+        tricktrack.game();
+    }
+}
+
+//ваодлдлдо
